e5a18b41309d4cdc6625e29a24130335f1929db4
Tomoki Morita <tmsongbooks215@gmail.com>
2021-06-12 01:26:05 +0900

    Remove web fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,12 +4,7 @@ export default class extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
