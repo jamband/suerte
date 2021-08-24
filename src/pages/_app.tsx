40c760a5f6b835ec43e5b91a6e65e9e7ab058287
Tomoki Morita <tmsongbooks215@gmail.com>
2021-08-25 02:06:26 +0900

    Add some contents for meta viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Notification } from "~/components/notification";
 import { NotificationProvider } from "~/contexts/notification";
 import { Layout } from "~/layouts/layout";
@@ -7,6 +8,12 @@ import "~/styles/app.scss";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+      </Head>
       <NotificationProvider>
         <Notification />
         <Component {...pageProps} />
