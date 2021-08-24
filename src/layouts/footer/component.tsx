5b1449e30accc7274277899fab0bbc79ebbfd63b
Tomoki Morita <tmsongbooks215@gmail.com>
2021-08-25 01:42:29 +0900

    Improve layout styles

--- src/layouts/footer/component.tsx
+++ src/layouts/footer/component.tsx
@@ -1,22 +1,26 @@
 import Link from "next/link";
 import { LinkExternal } from "~/components/link-external";
+import styles from "./style.module.scss";
 
 export const Component: React.VFC = () => (
   <footer className="fixed-bottom py-3 text-center bg-light">
-    <nav aria-label="Footer navigation">
+    <nav className={styles.contents} aria-label="Footer navigation">
       <ul className="list-inline m-0">
         <li className="list-inline-item">
           <Link href="/about">
-            <a>About</a>
+            <a className="px-3 py-2">About</a>
           </Link>
         </li>
         <li className="list-inline-item">
           <Link href="/contact">
-            <a>Contact</a>
+            <a className="px-3 py-2">Contact</a>
           </Link>
         </li>
         <li className="list-inline-item">
-          <LinkExternal href="https://github.com/jamband/suerte">
+          <LinkExternal
+            href="https://github.com/jamband/suerte"
+            className="px-3 py-2"
+          >
             GitHub
           </LinkExternal>
         </li>
--- src/layouts/header/component.tsx
+++ src/layouts/header/component.tsx
@@ -5,7 +5,7 @@ export const Component: React.VFC = () => (
   <header className="fixed-top py-3 bg-light">
     <div className="container text-center">
       <Link href="/">
-        <a className="text-decoration-none">{APP_NAME}</a>
+        <a className="px-3 py-2 text-decoration-none">{APP_NAME}</a>
       </Link>
     </div>
   </header>
