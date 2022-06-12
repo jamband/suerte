8ab58cd91ff90279dc5596ed0dbf9707a4f18908
Tomoki Morita <tmsongbooks215@gmail.com>
2022-06-12 23:45:27 +0900

    Add some scripts

---
+++ scripts/clean.mjs
@@ -0,0 +1,12 @@
+import { rmSync } from "fs";
+
+/** @type {import("fs").RmOptions} */
+const options = {
+  recursive: true,
+  force: true,
+};
+
+rmSync(".next", options);
+rmSync("node_modules", options);
+rmSync("storybook-static", options);
+rmSync("tsconfig.tsbuildinfo", options);
