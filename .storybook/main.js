4d4be74158efb399d47529de703df767815718c5
Tomoki Morita <tmsongbooks215@gmail.com>
2022-09-13 23:07:06 +0900

    Fix webpack settings for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+
 module.exports = {
   typescript: {
     reactDocgen: false,
@@ -20,4 +22,13 @@ module.exports = {
     },
     "storybook-addon-next-router",
   ],
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions,
+      }),
+    ];
+    return config;
+  },
 };
