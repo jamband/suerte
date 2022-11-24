b8269a161b7f4e70f339ae53c802ee8d8afc3891
Tomoki Morita <tmsongbooks215@gmail.com>
2022-11-25 02:28:38 +0900

    update clear command

--- scripts/clear.mjs
+++ scripts/clear.mjs
@@ -2,3 +2,4 @@ import { rm } from "node:fs/promises";
 
 await rm(".next", { recursive: true, force: true });
 await rm(".swc", { recursive: true, force: true });
+await rm("storybook-static", { recursive: true, force: true });
