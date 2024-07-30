a60929dd1cc5e9a9e4088ab0231c02813f75d002
Tomoki Morita <tmsongbooks215@gmail.com>
2024-07-30 16:44:29 +0900

    add copyright section in footer
    and some style changes

--- app/_layouts/footer/component.tsx
+++ app/_layouts/footer/component.tsx
@@ -1,23 +1,30 @@
+import { IconBoxArrowUpRight } from "@/_icons/box-arrow-up-right";
 import Link from "next/link";
 import styles from "./style.module.css";
 
 export const Component: React.FC = () => (
   <footer className={styles.container}>
-    <nav className={styles.navigation} aria-label="footer navigation">
-      <Link href="/about" className={styles.link}>
-        About
-      </Link>
-      <Link href="/contact" className={styles.link}>
-        Contact
-      </Link>
-      <a
-        href="https://github.com/jamband/suerte"
-        className={styles.link}
-        target="_blank"
-        rel="noreferrer"
-      >
-        GitHub
-      </a>
-    </nav>
+    <div className={styles.containerInner}>
+      <nav className={styles.navigation} aria-label="footer navigation">
+        <Link href="/about" className={styles.link}>
+          About
+        </Link>
+        <Link href="/contact" className={styles.link}>
+          Contact
+        </Link>
+        <a
+          href="https://github.com/jamband/suerte"
+          className={styles.link}
+          rel="noreferrer"
+          target="_blank"
+        >
+          GitHub
+          <IconBoxArrowUpRight className={styles.linkIcon} />
+        </a>
+      </nav>
+      <div className={styles.copyright}>
+        ©️ {new Date().getFullYear()} Tomoki Morita
+      </div>
+    </div>
   </footer>
 );
