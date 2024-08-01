71e9c81b34c32f43622d3774f3fcfcdefe2d30b4
Tomoki Morita <tmsongbooks215@gmail.com>
2024-08-01 23:03:06 +0900

    add storybook link on header
    and some style improvements

--- app/_layouts/header/component.tsx
+++ app/_layouts/header/component.tsx
@@ -1,13 +1,23 @@
-import { APP_NAME } from "@/_constants/app";
+import { APP_NAME, APP_STORYBOOK_URL } from "@/_constants/app";
+import { IconStorybook } from "@/_icons/storybook";
 import Link from "next/link";
 import styles from "./styles.module.css";
 
 export const Component: React.FC = () => (
   <header className={styles.container}>
     <nav className={styles.navigation} aria-label="header navigation">
-      <Link href="/" className={styles.link}>
+      <Link href="/" className={styles.brandLink}>
         {APP_NAME}
       </Link>
+      <a
+        href={APP_STORYBOOK_URL}
+        className={styles.storybookLink}
+        rel="noreferrer"
+        target="_blank"
+      >
+        <IconStorybook className={styles.storybookLinkIcon} />
+        Storybook
+      </a>
     </nav>
   </header>
 );
